e0fa58ceb702c85b7e51fd6deb722066638c2834
kazuma hanada <hndkzm1990@gmail.com>
2018-11-06 20:34:58 +0900

    implement initSource

--- getSource.c
+++ getSource.c
@@ -1,9 +1,51 @@
 #include<stdio.h>
 #include<string.h>
+#include"getSource.h"
 
 
 static FILE *fpi;
 //static FILE *fptex;
+
+
+static int  lineIndex;
+static char ch;
+
+
+static KeyId charClassT[256];
+
+
+static void initCharClassT()
+{
+	int i;
+	
+	//とりあえず、全ての配列にotherを代入
+	for(i=0; i<256; i++)
+		charClassT[i] = others;
+	
+	//ASCⅡコードに基づいた添字を使う
+	for(i='0'; i<='9';i++)
+		charClassT[i] = digit;
+	
+	for(i='A'; i<='Z'; i++)
+	    charClassT[i] = letter;
+	
+	for(i='a'; i<='z'; i++)
+		charClassT[i] = letter;
+	
+	charClassT['+'] = Plus;
+	charClassT['-'] = Minus;
+	charClassT['*'] = Mult;
+	charClassT['/'] = Div;
+	charClassT['('] = Lparen;
+	charClassT[')'] = Rparen;
+	charClassT['='] = Equal;
+	charClassT['<'] = Lss;
+	charClassT['>'] = Gtr;
+	charClassT[','] = Comma;
+	charClassT['.'] = Period;
+	charClassT[';'] = Semicolon;
+	charClassT[':'] = colon;
+}
 
 
 int openSource(char fileName[])
@@ -35,4 +77,23 @@ void closeSource()
 	fclose(fpi);
 	//fclose(fptex);	
 	printf("close\n");
+}
+
+
+void initSource()
+{
+	lineIndex = -1;
+	ch = '\n';
+	//printed = 1;
+	initCharClassT();
+	
+	/*
+	//LaTeXコマンド
+	fprintf(fptex,"\\documentstyle[12pt]{article}\n");   
+	fprintf(fptex,"\\begin{document}\n");
+	fprintf(fptex,"\\fboxsep=0pt\n");
+	fprintf(fptex,"\\def\\insert#1{$\\fbox{#1}$}\n");
+	fprintf(fptex,"\\def\\delete#1{$\\fboxrule=.5mm\\fbox{#1}$}\n");
+	fprintf(fptex,"\\rm\n");
+	*/
 }
